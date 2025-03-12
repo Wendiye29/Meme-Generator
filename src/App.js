720607675b78e6f74209bd72cfeb19e1cf0a0618
Wendiye29 <girmasossina2@gmail.com>
2025-03-12 14:40:35 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Generating from "./Meme"
+function App(){
+  return(
+<Generating />
+  )
+}
+export default App
---
+++ src/Meme.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react";
+import a from "./Imgg/1.jpg"
+
+//import c from "./Imgg/3.png"
+import df from "./Imgg/4.jpg"
+import e from "./Imgg/5.jpg" 
+import f from "./Imgg/6.jpg"
+class Generating extends Component{
+constructor(){
+    super()
+    this.state={
+        topText:"",
+        bottomText: "",
+        imageIndex:0,
+        allImages:[
+            a, df, e, f
+        ]
+    }
+    this.Changing=this.Changing.bind(this)
+    this.txtChanging=this.txtChanging.bind(this)
+}
+Changing(event){
+event.preventDefault()
+this.setState((prevState =>({
+    imageIndex:(prevState.imageIndex + 1) % prevState.allImages.length
+})))
+}
+txtChanging(event){
+    this.setState({
+[event.target.name]:event.target.value
+    }
+)
+}
+render(){
+    return(
+        <form onSubmit={this.Changing}>
+            <input type="text"
+            name="topText"
+            placeholder="top Text"
+            onChange={this.txtChanging} />
+            <input type="text"
+            name="bottomText"
+            placeholder="bottom Text"
+            onChange={this.txtChanging} 
+            />
+            <button >Generate</button>
+            <div>
+                <h3>{this.state.topText}</h3>
+                <img className="img" src={this.state.allImages[this.state.imageIndex]}
+                alt="picture"/>
+                <h3 h>{this.state.bottomText}</h3>
+            </div>
+        </form>
+    )
+}
+}
+export default Generating
